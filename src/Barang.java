7f3505510d123521df0916e34ac8cf158602217d
wandi1209 <wandider@gmail.com>
2025-04-15 09:48:19 +0700

    membuat class barang

---
+++ src/Barang.java
@@ -0,0 +1,19 @@
+public class Barang {
+    String kodeBarang, nama, kategori;
+    int stok, harga;
+
+    Barang(String kodeBarang, String nama, String kategori, int stok, int harga){
+        this.kodeBarang = kodeBarang;
+        this.nama = nama;
+        this.kategori = kategori;
+        this.stok = stok;
+        this.harga = harga;
+    }
+
+    void tampilDataBarang(Barang[] barang){
+        System.out.printf("%-15s %-15s %-15s %-15s %-15s", "Kode Barang", "Nama Barang", "Kategori", "Stok", "Harga");
+        for (Barang brg : barang) {
+            System.out.printf("\n%-15s %-15s %-15s %-15d %-15d\n", brg.kodeBarang, brg.nama, brg.kategori, brg.stok, brg.harga);
+        }
+    }
+}
