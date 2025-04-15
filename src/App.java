1804a9c69076fc7f18df62c945855e70753d308b
Indhirayc <indhirayuantikac1176@gmail.com>
2025-04-15 10:38:05 +0700

    Menu transaksi pembelian

--- src/App.java
+++ src/App.java
@@ -9,17 +9,23 @@ public class App {
         barangs[2] = new Barang("Br003", "Teh Pucuk Botol", "Minuman", 10, 4000);
         barangs[3] = new Barang("Br004", "Rinso Cair", "Sabun", 10, 7000);
         barangs[4] = new Barang("Br005", "Mama Lemon Cair", "Sabun", 10, 1000);
-
+        
+        TransaksiPembelian[] transaksiPembelians = new TransaksiPembelian[5];
+        transaksiPembelians[0] =new TransaksiPembelian("Tr001", "Santi", "21-04-2024", 1, barangs[0]);
+        transaksiPembelians[1] =new TransaksiPembelian("Tr002", "Vani", "21-04-2024", 2, barangs[1]);
+        transaksiPembelians[2] =new TransaksiPembelian("Tr003", "Siska", "21-04-2024", 3, barangs[2]);
+        transaksiPembelians[3] =new TransaksiPembelian("Tr004", "MeiMei", "21-04-2024", 1, barangs[3]);
+        transaksiPembelians[4] =new TransaksiPembelian("Tr005", "Ihsan", "21-04-2024", 1, barangs[4]);
 
         while (true) { 
-            System.out.println("========== TOKO MANASUKA ==========");
+            System.out.println("\n========== TOKO MANASUKA ==========");
             System.out.println("BANK MENU :");
             System.out.println("1. Data Barang");
             System.out.println("2. Data Transaksi");
             System.out.println("3. Pencarian Data");
             System.out.println("4. Pengurutan Data");
             System.out.println("5. Keluar Menu");
-            System.out.println("Pilih Menu (1-5) : ");
+            System.out.print("Pilih Menu (1-5) : ");
             int plhmn = input.nextInt();
             input.nextLine();
 
@@ -27,7 +33,9 @@ public class App {
                 case 1 -> {
                     barangs[0].tampilDataBarang(barangs);
                 }
-                case 2 -> {}
+                case 2 -> {
+                    transaksiPembelians[0].tampilDataTransaksi(transaksiPembelians);
+                }
                 case 3 -> {}
                 case 4 -> {}
                 case 5 -> {
