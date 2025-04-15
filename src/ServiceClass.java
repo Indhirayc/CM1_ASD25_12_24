b978e946d3310edff2c0de18c9ee3fc9840b3664
wandi1209 <wandider@gmail.com>
2025-04-15 10:54:54 +0700

    membuat service class

---
+++ src/ServiceClass.java
@@ -0,0 +1,13 @@
+public class ServiceClass {
+    TransaksiPembelian[] transaksiPembelian;
+    int index;
+
+    ServiceClass(TransaksiPembelian[] transaksiPembelian, int index){
+        this.transaksiPembelian = transaksiPembelian;
+        this.index = index;
+    }
+
+    void searching(){}
+
+    void sorting(){}
+}
